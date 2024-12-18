f5f28f09a8a7d06de0738d8302b715ebf32093b8
alaicazaragoza <alaicazaragoza@gmail.com>
2024-12-18 09:59:07 +0800

    Add hosted link in cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://blog-app-client-zaragoza.vercel.app'],
     credentials: true,
     optionsSuccessStatus: 200
 };
